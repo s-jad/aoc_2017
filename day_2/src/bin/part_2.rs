bcea187881a4f834c0b16e346a41e99aa38aad10
s-jad <s_jad@web.de>
2024-01-22 11:06:46 +0100

    Solved day 2 part 2- corrected

--- day_2/src/bin/part_2.rs
+++ day_2/src/bin/part_2.rs
@@ -1,2 +1,47 @@
+use itertools::Itertools;
+use std::time::Instant;
 
+fn process(input: &str) -> usize {
+    input
+        .lines()
+        .filter_map(|l| {
+            let nums = l
+                .split_whitespace()
+                .into_iter()
+                .filter_map(|s| s.parse::<usize>().ok())
+                .collect_vec();
 
+            let divisible = nums
+                .into_iter()
+                .combinations(2)
+                .filter(|v| v[0] != v[1])
+                .find(|v| v[0].max(v[1]) % v[0].min(v[1]) == 0);
+
+            divisible
+        })
+        .map(|d| d[0].max(d[1]) / d[0].min(d[1]))
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
