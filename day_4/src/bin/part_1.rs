a460c39ebe00f05af1757cd7b7ca7b022876fa1e
s-jad <s_jad@web.de>
2024-01-22 14:16:40 +0100

    Solved day 4 part 1

---
+++ day_4/src/bin/part_1.rs
@@ -0,0 +1,41 @@
+use itertools::Itertools;
+use std::{collections::HashSet, time::Instant};
+
+fn process(input: &str) -> usize {
+    input
+        .lines()
+        .map(|l| {
+            let parts = l.split_whitespace().collect_vec();
+            let p_set = parts.iter().collect::<HashSet<&&str>>();
+
+            if parts.len() == p_set.len() {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
