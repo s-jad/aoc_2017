c488b591fe89140c717ac1549a2f80bc8fdca27a
s-jad <s_jad@web.de>
2024-01-23 13:06:14 +0100

    Solved day 8 part 1

---
+++ day_8/src/bin/part_1.rs
@@ -0,0 +1,83 @@
+use itertools::Itertools;
+use std::{collections::HashMap, time::Instant};
+
+fn parse_instruction<'a>(
+    mut registers: HashMap<&'a str, i32>,
+    line: &'a str,
+) -> HashMap<&'a str, i32> {
+    let parts = line.split_whitespace().collect_vec();
+    let (r1, r2) = (parts[0], parts[4]);
+
+    match (registers.contains_key(r1), registers.contains_key(r2)) {
+        (true, false) => {
+            registers.insert(r2, 0);
+        }
+        (false, true) => {
+            registers.insert(r1, 0);
+        }
+        (false, false) => {
+            registers.insert(r1, 0);
+            registers.insert(r2, 0);
+        }
+        _ => {}
+    }
+    let (r, p) = (
+        registers.get(r2).unwrap(),
+        &parts[6].parse::<i32>().expect("parts[6] is num)"),
+    );
+
+    let cond = match parts[5] {
+        "<" => r < p,
+        ">" => r > p,
+        "==" => r == p,
+        "!=" => r != p,
+        "<=" => r <= p,
+        ">=" => r >= p,
+        _ => unreachable!(),
+    };
+
+    if cond {
+        let ins = match parts[1] {
+            "inc" => parts[2].parse::<i32>().unwrap(),
+            "dec" => -(parts[2].parse::<i32>().unwrap()),
+            _ => unreachable!(),
+        };
+
+        let num = registers.get_mut(&r1).unwrap();
+        *num += ins;
+    }
+
+    registers
+}
+
+fn process(input: &str) -> i32 {
+    let registers = input.lines().fold(HashMap::new(), |registers, line| {
+        let new_registers = parse_instruction(registers, line);
+        new_registers
+    });
+
+    *registers.values().max().unwrap()
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
