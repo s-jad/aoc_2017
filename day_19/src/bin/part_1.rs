1af68987cc5fa72496646b79e57918a0c7b43459
s-jad <s_jad@web.de>
2024-01-25 12:48:35 +0100

    Solved day 19 part 1

---
+++ day_19/src/bin/part_1.rs
@@ -0,0 +1,87 @@
+use itertools::Itertools;
+use std::{collections::HashMap, time::Instant};
+
+fn process(input: &str) -> String {
+    let (path, first) = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, s)| {
+            s.chars()
+                .enumerate()
+                .filter(|(_, c)| !c.is_whitespace())
+                .map(|(x, c)| ((x, y), c))
+                .collect_vec()
+        })
+        .fold(
+            (HashMap::new(), (1000i32, 1000i32)),
+            |(mut path, mut first), ((x, y), c)| {
+                path.insert((x as i32, y as i32), c);
+
+                if c == '|' && y == 0 {
+                    first = (x as i32, y as i32);
+                }
+
+                (path, first)
+            },
+        );
+
+    let mut letters = String::new();
+    let mut c = path.get(&first);
+    let mut cpos = first;
+    let mut dx = 0;
+    let mut dy = 1;
+
+    while c.is_some() {
+        let unwrapped = c.unwrap();
+
+        match unwrapped {
+            ch if ch.is_alphabetic() => {
+                letters.push(*ch);
+            }
+            '+' => {
+                let neighbours = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+                    .iter()
+                    .filter(|(nx, ny)| !(nx == &(-dx) && ny == &(-dy)))
+                    .collect_vec();
+
+                for (nx, ny) in neighbours {
+                    let n = path.get(&(cpos.0 + nx, cpos.1 + ny));
+                    if n.is_some() {
+                        dx = *nx;
+                        dy = *ny;
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        let next = (cpos.0 + dx, cpos.1 + dy);
+        c = path.get(&next);
+        cpos = next;
+    }
+
+    letters
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let start = Instant::now();
+    let output = process(input);
+    let time = start.elapsed();
+
+    dbg!(output, time);
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = include_str!("../../test.txt");
+        let output = process(input);
+        assert_eq!(result,);
+    }
+}
